1e8a538e0b94bb07481fd418cda47341b1eb0236
ghsr <ghsr92@yandex.ru>
2015-03-27 22:22:45 +0600

    rhea: Show percentage in charger mode
    Conflicts:
    	BoardConfigCommon.mk

--- BoardConfigCommon.mk
+++ BoardConfigCommon.mk
@@ -92,6 +92,7 @@ EXTENDED_FONT_FOOTPRINT := true
 
 # Charger
 BOARD_CHARGER_ENABLE_SUSPEND := true
+BOARD_CHARGER_SHOW_PERCENTAGE := true
 BOARD_CHARGING_MODE_BOOTING_LPM := /sys/class/power_supply/battery/batt_lp_charging
 
 # Connectivity - Wi-Fi
