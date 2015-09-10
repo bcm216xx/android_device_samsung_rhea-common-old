ce89a10b799df92aa651120ecf0eadc06b73b48f
ghsr <ghsr92@yandex.ru>
2015-09-10 12:16:17 +0600

    rhea: media: Switch to lower-end codecs
    Conflicts:
    	configs/etc/media_codecs.xml

--- rhea.mk
+++ rhea.mk
@@ -95,7 +95,7 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     frameworks/av/media/libstagefright/data/media_codecs_google_audio.xml:system/etc/media_codecs_google_audio.xml \
     frameworks/av/media/libstagefright/data/media_codecs_google_telephony.xml:system/etc/media_codecs_google_telephony.xml \
-    frameworks/av/media/libstagefright/data/media_codecs_google_video.xml:system/etc/media_codecs_google_video.xml \
+    frameworks/av/media/libstagefright/data/media_codecs_google_video_le.xml:system/etc/media_codecs_google_video_le.xml \
     $(COMMON_PATH)/configs/etc/media_codecs.xml:system/etc/media_codecs.xml \
     $(COMMON_PATH)/configs/etc/media_profiles.xml:system/etc/media_profiles.xml \
     $(COMMON_PATH)/configs/etc/audio_policy.conf:system/etc/audio_policy.conf 
