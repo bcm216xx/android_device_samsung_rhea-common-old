4c570142bfeb2b3d874e399389c1e843e287a92d
Karthick C <karthick111@users.noreply.github.com>
2016-11-23 22:40:08 +0530

    rhea: dalvik.vm.dex2oat-filter=interpret-only

--- rhea.mk
+++ rhea.mk
@@ -177,10 +177,10 @@ ADDITIONAL_DEFAULT_PROPERTIES += \
 
 # ART
 PRODUCT_PROPERTY_OVERRIDES += \
-    dalvik.vm.dex2oat-filter=balanced \
+    dalvik.vm.dex2oat-filter=interpret-only \
     dalvik.vm.dex2oat-flags=--no-watch-dog \
     dalvik.vm.image-dex2oat-filter=speed
-
+    
 # MTP
 PRODUCT_DEFAULT_PROPERTY_OVERRIDES += \
     persist.sys.usb.config=mtp
